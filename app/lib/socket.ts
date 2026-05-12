df63711ee3b17daa338cfb8678c93ab10ecdbd38
oyoon <avcap1234@naver.com>
2026-05-12 11:53:21 +0900

    배포용 코드 수정

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // 서버 주소 (Railway 배포 전이라면 localhost:5000)
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "http://st:5000";
 
 export const socket = io(SOCKET_URL, {
     autoConnect: true,
